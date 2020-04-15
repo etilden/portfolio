efe5a0dab3efcf16561c05dc3754e2569efedd8e
Emma Tilden <e.k.tilden@gmail.com>
2020-04-14 22:26:58 -0500

    Updates

---
+++ precache-manifest.1f8cdb629aacd21800a6ad2a6a3c45dc.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49f9d6433c2d61a8cc3f0d0cccda01d0",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "77362cdea06e8f258d65",
+    "url": "/portfolio/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "9d6fa0c15a17a6c228a0",
+    "url": "/portfolio/static/css/main.5923d0ce.chunk.css"
+  },
+  {
+    "revision": "77362cdea06e8f258d65",
+    "url": "/portfolio/static/js/2.524a9858.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.524a9858.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d6fa0c15a17a6c228a0",
+    "url": "/portfolio/static/js/main.61140af6.chunk.js"
+  },
+  {
+    "revision": "722a7b1787f3dfbf48e8",
+    "url": "/portfolio/static/js/runtime-main.558dd139.js"
+  },
+  {
+    "revision": "3fc6c99003e7a61817d7ec277b355ad6",
+    "url": "/portfolio/static/media/ahoy.3fc6c990.png"
+  },
+  {
+    "revision": "228ff54730deb11c7b48ab56a781388e",
+    "url": "/portfolio/static/media/background(2).228ff547.png"
+  },
+  {
+    "revision": "1d4ef83129b42cfa2f35edd2fe962679",
+    "url": "/portfolio/static/media/cubeClicker.1d4ef831.png"
+  },
+  {
+    "revision": "f3620c5a6fa56afa6ce3f8e52f1c6253",
+    "url": "/portfolio/static/media/markdownEditor.f3620c5a.png"
+  },
+  {
+    "revision": "67016dc5b2a73fabfdbcd04f340a10ec",
+    "url": "/portfolio/static/media/omnivoreStore.67016dc5.png"
+  },
+  {
+    "revision": "decfea4804750b456deb326c7c3c1ece",
+    "url": "/portfolio/static/media/square_.decfea48.png"
+  }
+]);
